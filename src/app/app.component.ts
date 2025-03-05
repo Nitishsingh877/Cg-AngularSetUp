a49ddf849f0a20fdaad9050f77402e778ba86da9
Nitish singh <nitishkumarsingh877@gmail.com>
2025-03-05 23:17:42 +0530

    [Nitish] To display Hello from BridgeLabz with a user inputted name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports:[FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'HelloWorld';
   imgUrl = "assets/BL_logo_square_jpg.jpg";
-  url = "https://www.bridgelabz.com"
+  url = "https://www.bridgelabz.com";
+  userName: string = '';
 
 
   ngOnInit() : void {
-    this.title = "Hello from BridgeLabz."
+    this.title = "Hello from BridgeLabz Mr/mrs."
   }
   onClick(event: MouseEvent): void {
     console.log("Save button is clicked", event);
